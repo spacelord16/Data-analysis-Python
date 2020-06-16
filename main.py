bd5d61756555bd7e7a2d99041df859a12402656d
Aditya <deshpande.aditya16@gmail.com>
2020-06-16 20:45:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+from pandas_profiling import ProfileReport
+
+df = pd.read_csv('housing.csv')
+print(df)
+
+
+# Generate a report
+profile = ProfileReport(df)
+profile.to_file(output_file="housing.html")
